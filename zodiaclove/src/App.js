5d6da8408b5c226b7f3e1290eb1706818fe99e2a
888Lucy888 <chuggaaconroy888@gmail.com>
2021-11-10 16:03:52 -0600

    grid

--- zodiaclove/src/App.js
+++ zodiaclove/src/App.js
@@ -16,7 +16,7 @@ function App() {
       <header className="App-header">
         <MatchCards name="L" desc="Hello my name is L" zSign="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" img="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"/>
         <CompatCard zSign1="Leo" zSign2="Leo" desc="Leo and Leo are both fire signs, they go well with eachother since the more fire the better" img="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"/>
-        <CompatGrid img="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" info={info}/>
+        <CompatGrid img="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" info={""/*info*/}/>
       </header>
     </div>
   );
