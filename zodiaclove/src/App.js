e3bd116b1c8134d20ad1bb4758d5c2f9b80b16e5
eduardogora <eduardogoracedi@outlook.com>
2021-11-15 19:27:58 -0600

    storeComponent

--- zodiaclove/src/App.js
+++ zodiaclove/src/App.js
@@ -4,14 +4,16 @@ import 'antd/dist/antd.css';
 import UserData from "./api/UserData";
 import Navbar from "./Components/Navbar";
 import HoroscopeCard from "./Components/HoroscopeCard";
+import Store from "./Components/store";
 
 function App() {
   return (
     <div className="App">
       <Navbar>
       </Navbar>
-      <HoroscopeCard>
-      </HoroscopeCard>
+      <Store>
+      </Store> 
+
     </div>
   );
 }
