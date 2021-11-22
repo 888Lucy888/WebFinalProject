d237451b820416510dac0ea7a01d993432de4366
888Lucy888 <chuggaaconroy888@gmail.com>
2021-11-21 19:15:06 -0600

    matches

--- zodiaclove/src/Components/MatchCard.jsx
+++ zodiaclove/src/Components/MatchCard.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Card, Avatar } from 'antd';
 import { HeartTwoTone, CloseCircleTwoTone } from '@ant-design/icons';
+import { deleteModel } from 'mongoose';
 
 const { Meta } = Card;
 
 function MatchCard(props){
-    const {img, zSign, name, desc} = props;
+    const {img, zSign, name, desc, match, remove} = props;
     return(
         <div>
             <Card
-                style={{ width: 300, backgroundColor: "#edeaff", border:0, color: "white"}}
-                bodyStyle={{backgroundColor: "#edeaff",border:0, color: "aliceblue"}}
+                style={{ width: 300, backgroundColor: "#f0f2ff", border:0, color: "white", margin:"auto", marginTop:"4%"}}
+                bodyStyle={{backgroundColor: "#d2dbff",border:0, color: "aliceblue"}}
                 cover={
                     <img alt="ProfilePic" src={img}/>
                 }
                 actions={[
-                    <HeartTwoTone key="add" twoToneColor="#954ce9" />,
-                    <CloseCircleTwoTone key="del" twoToneColor="#56b9ca" />,
+                    <HeartTwoTone key="add" twoToneColor="#836fff" onClick={match} />,
+                    <CloseCircleTwoTone key="del" twoToneColor="#394692" onClick={remove}/>,
                 ]}
             >
             <Meta   
                 style={{fontSize:14}}
-                avatar={<Avatar src={zSign} />}
-                title={name}
+                avatar={<Avatar src={"./zodiacImgs/"+String(zSign).toLowerCase()+".jpg"} />}
+                title={name+" - "+zSign}
                 description={desc}
             />
             </Card>
--- zodiaclove/src/Views/matchesView.jsx
+++ zodiaclove/src/Views/matchesView.jsx
@@ -1,21 +1,55 @@
-//import "./App.css";
 import 'antd/dist/antd.css';
-
 import React, { Component } from "react"; 
-
-//import UserData from "./api/UserData";
-import ProfileCards from "../Components/ProfileCards";
+import MatchCard from './../Components/MatchCard';
 
 class MatchesView extends Component {
-  state = {};
+  state = {
+    matches:[
+      {
+        name:"Joe",
+        desc:"Hi I'm hot",
+        img:"https://i.pinimg.com/originals/28/d3/83/28d3837232911297b40210071de67297.png",
+        sign:"Aries",
+        id:"1232"
+      },
+      {
+        name:"Sally",
+        desc:"I like painting",
+        img:"https://www.imagediamond.com/blog/wp-content/uploads/2020/06/cartoon-girl-dp-images-5.jpg",
+        sign:"Libra",
+        id:"23298"
+      }
+    ]
+  };
 
-  async componentDidMount() {}
+  async componentDidMount(
+
+  ) {}
+
+  match = () =>{
+    //Send email
+  }
+
+  remove = (id)=> {
+    this.setState({
+      matches: this.state.matches.filter(item => item.id !== id)
+    });
+    console.log(this.state.matches);
+  }
 
   render() {
+    let matchData = this.state && this.state.matches.length > 0 ?
+            this.state.matches.map(p=>
+                <MatchCard zSign={p.sign} name={p.name} desc={p.desc} img={p.img} match={()=>this.match()} remove={()=>this.remove(p.id)}/>
+            ) : <span style={{margin:"3%", color:"aliceblue", fontSize:20}}> Oops! It appears you are out of matches</span>;
+
     return (
-        <div>
-            <h1>matchesView</h1>
+      <div>
+        <h1 style={{margin:"3%", fontSize:32, color:"#1a1e55"}}>The Universe might like...</h1>
+        <div style={{padding:"3%", backgroundColor:"#786094"}}>
+          {matchData}
         </div>
+      </div>
     );
   }
 }
